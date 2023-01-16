494d54ebcd54149eda89236f726ce60e9d9bf2f4
pokabilius <georgemirsilidis@hotmail.com>
2023-01-17 01:05:09 +0200

    make loop

--- main.js
+++ main.js
@@ -1,45 +1,104 @@
+//play for 5 times and count wins
+
+// Function to generate computer's choice -> returns rock papper or sciscors
+
+let array = ["rock", "papper", "scissors"];
 function generateComputerGuess (){
-    let array = ["rock", "papper", "scissors"];
-    randomNumber = Math.floor(Math.random() * 3)
     // console.log(array[randomNumber])
-    return (array[randomNumber])
+    randomNumber = Math.floor(Math.random() * 3);
     
+    return (array[randomNumber]);
     
 }
 
-let computerGuess = generateComputerGuess();
-console.log(`pc chose ${computerGuess}`)
+// let computerGuess = generateComputerGuess(array);
+// console.log(`pc chose ${computerGuess}`);
 
 
-function playerGuess (){
+//function to prompt user for choice - tolowercase
+
+function playerPrompt (){
     let guess = prompt("Choose Rock Paper or Scissors")
-
-    //turn guess in lower
+    
+    //turn guess in lowerCase
     let finalGuess = guess.toLowerCase();
-    // console.log(finalGuess)
-    return guess; 
+    return finalGuess;
 }
 
-let playerChoice = playerGuess();
-console.log(`player chose ${playerChoice}`);
+// let playerChoice = playerPrompt();
+// console.log(`player chose ${playerChoice}`);
 
+// function to check for legit input
+
+function checkUseriInput(playerChoice){
+    let check = false
+
+    while (check === false){
+        if (playerChoice === "papper" || playerChoice === "rock" || playerChoice === "scissors"){
+            check = true;
+            return playerChoice
+        }
+        else {
+            playerChoice = prompt("Please enter a correct answer");
+            playerChoice = playerChoice.toLowerCase();
+            
+        }
+        
+    }
+}
+
+// let finalAnswer = checkUseriInput(playerChoice);
+// console.log(finalAnswer);
+
+
+
+//function to compare user input with pc choice and return winner
 
 function play(pc, player){
-    if(pc === player){
-        console.log("You have a draw")
-    }
-    else if(pc === "paper" && player === "scissors"){
-        console.log("You won")
-    }
-    else if(pc === "scissors" && player === "rock"){
-        console.log("You won")
-    }
-    else if(pc === "rock" && player === "paper"){
-        console.log("You won")
-    }
-    else{
-        console.log("You lost")
-    }
+        
+        let winner;
+        let pcWIns = 0
+        let playerWins = 0
+        if(pc === player){
+            console.log("You have a draw")
+            winner = "draw"
+        }
+        else if(pc === "paper" && player === "scissors"){
+            console.log("You won")
+            winner = "player"
+            playerWins++
+        }
+        else if(pc === "scissors" && player === "rock"){
+            console.log("You won")
+            winner = "player"
+            playerWins++
+        }
+        else if(pc === "rock" && player === "paper"){
+            console.log("You won")
+            winner = "player"
+            playerWins++
+        }
+        else{
+            console.log("You lost")
+            winner = "pc"
+            pcWIns++
+        }
+    console.log(`winner is ${winner}`)
+    return pcWIns;
 }
 
-play(computerGuess, playerChoice);
+let i = 0 
+
+let wins;
+while(i<2){
+    let computerGuess = generateComputerGuess(array);
+    console.log(`computer is ${computerGuess}`)
+    let playerChoice = playerPrompt();
+    let finalAnswer = checkUseriInput(playerChoice);
+    let wins = play(computerGuess, finalAnswer)
+    i++
+}
+
+console.log(`Pc won ${wins}`)
+//-------------------working above------------------------------------------------
+
